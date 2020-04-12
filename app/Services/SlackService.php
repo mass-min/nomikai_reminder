8d313a8a657fb28900a412f270fc79bc9a7fb37e
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-04-12 17:20:17 +0900

    どこにツイートするか決めるためのもの

--- app/Services/SlackService.php
+++ app/Services/SlackService.php
@@ -2,13 +2,25 @@
 
 namespace App\Services;
 
+use Abraham\TwitterOAuth\TwitterOAuth;
 use Illuminate\Support\Facades\Http;
 
 class SlackService
 {
+    protected $channelId;
+
+    public function loadPostChannelId(string $channelName)
+    {
+        $response = Http::get('https://slack.com/api/channels.list?token=' + env('SLACK_BOT_ACCESSTOKEN'));
+        $channelInfos = $response->json();
+        $response = Http::get('https://slack.com/api/groups.list?token=' + env('SLACK_BOT_ACCESSTOKEN'));
+        $groupInfos = $response->json();
+        $this->channelId = $response->json();
+        echo json_encode($response->json());
+    }
+
     public function tweetOnlineMtgInfo(string $tweet)
     {
-        // ツイートする
         echo $tweet . "\n";
     }
 }
